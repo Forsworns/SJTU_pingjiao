02624b3f08586d597567c5cf8e1114a1b0452663
Forsworn <378974295@qq.com>
2018-05-28 01:27:51 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # SJTU_pingjiao
-评教懒人脚本
+## 评教懒人脚本
+
+* 适用于新版评教网http://pingjiao.sjtu.edu.cn
+* 基于Tampermonkey
+* 进入新版评教网“我的调查”页面，即http://pingjiao.sjtu.edu.cn/#/my-events/xx格式url页面，启用Tampermonkey，按一下F5重新加载页面启用脚本，之后可挂机至alert窗口提示评教完成
+* 可能还有bug，但是自己的评教机会已经测试完了，明天起床用舍友的测试一下
---
+++ source.js
@@ -0,0 +1,61 @@
+// ==UserScript==
+// @name         SJTU_class_survey
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @author       Forsworn
+// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.18.2/babel.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.16.0/polyfill.js
+// @require      https://code.jquery.com/jquery-3.3.1.min.js
+// @match        <$URL$>
+// @run-at document-idle
+// @include http://pingjiao.sjtu.edu.cn/*
+// ==/UserScript==
+
+(function() {
+    let evaluationWords = ["非常认同","难度适当","基本都在听课","非常有兴趣","提升很多"];
+    function sleep(numberMillis) {
+        var now = new Date()
+        var exitTime = now.getTime() + numberMillis
+        while (true) {
+            now = new Date()
+            if (now.getTime() > exitTime) return
+        }
+    }
+    function fill(){
+        $(document).ready(function(){
+            let options = $(".option-container")
+            for(let item of options){
+                if($.inArray(item.innerText,evaluationWords) !== -1){
+                    $(item).siblings("td").children("div").children("span").click()
+                }
+            }
+            sleep(5000)
+            $(".paper-submit").click()
+            sleep(500)
+            $(".btn-back")[0].click()
+            window.onload = survey()
+        })
+    }
+    function survey(){
+        $(document).ready(function(){
+            let boxes = $(".course-state.active")
+            if(boxes.length){
+                boxes[0].click()
+                window.location.reload()
+            }else{
+                alert("done!")
+            }
+        })
+    }
+    if(window.location.href.indexOf("my-events") !== -1) window.onload = survey()
+    else if(window.location.href.indexOf("answer-paper") !== -1) window.onload = fill()
+})();
+
+/*
+let url = window.location.href
+let reg = /(\/[0-9]+){3}$/
+url = url.match(reg)[0]
+url = url.slice(url.lastIndexOf("/"),url.length) + url.slice(0,url.lastIndexOf("/"))
+console.log(url)
+$.get(`http://pingjiao.sjtu.edu.cn/api/question-paper${url}`).done(()=>{});
+改进：利用这个get直接整理出反馈然后直接post?*/
